a9f0fd3e5b52e82a3c55c42440af8f6411927236
Milena Mariz <milenaferreira294@hotmail.com>
2024-05-04 19:25:27 -0300

    feat: move two paddles with different keys

--- Pong/scripts/paddle.gd
+++ Pong/scripts/paddle.gd
@@ -2,8 +2,11 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
+@export var move_up : StringName
+@export var move_down : StringName
+
 func _physics_process(_delta):
-	var direction = Input.get_axis("ui_up", "ui_down")
+	var direction = Input.get_axis(move_up, move_down)
 	if direction:
 		velocity.y = direction * SPEED
 	else:
