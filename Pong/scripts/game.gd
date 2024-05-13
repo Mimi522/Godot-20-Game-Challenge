e235cf3b0c5ffa5789f44707f70f7a0d0c43bbd2
Milena Mariz <milenaferreira294@hotmail.com>
2024-05-13 19:01:51 -0300

    feat: add score tracking on board

--- Pong/scripts/game.gd
+++ Pong/scripts/game.gd
@@ -2,19 +2,36 @@ extends Node2D
 
 @export var ball_scene: PackedScene
 
+const score = "%s : %s"
+
+var player1_score := 0
+var player2_score := 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	update_score()
 	spawn_ball()
-	
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	pass
+	update_score()
 
-func _on_dead_zone_body_entered(body):
-	body.queue_free()
-	spawn_ball()
-	
+func _on_dead_zone_body_entered_left(body):
+	player2_score += 1
+	despawn_ball(body)
+
+func _on_dead_zone_body_entered_right(body):
+	player1_score += 1
+	despawn_ball(body)
+
 func spawn_ball():
 	var ball = ball_scene.instantiate()
 	ball.position = get_viewport_rect().size / 2
 	add_child(ball)
+
+func despawn_ball(body):
+	body.queue_free()
+	spawn_ball()
+
+func update_score():
+	$Score.text = score % [player1_score, player2_score]
