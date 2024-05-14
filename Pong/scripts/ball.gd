10ce433e5d99ee1f5336b97af1c824fa66a09939
Milena Mariz <milenaferreira294@hotmail.com>
2024-05-13 23:51:29 -0300

    fix: bouncing mechanics close #1
    OH MY GOD THERE IS A BOUNCE FUNCTION!!!!!! O.O
    I feel so stupid...

--- Pong/scripts/ball.gd
+++ Pong/scripts/ball.gd
@@ -6,14 +6,10 @@ var speed = 500
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var angle = randf_range(-PI / 4, PI / 4)
-	direction = Vector2(cos(angle), sin(angle))
+	set_velocity(Vector2(cos(angle) * speed * [-1, 1].pick_random(), sin(angle) * speed))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var collision = move_and_collide(direction * delta * speed)
+	var collision = move_and_collide(velocity * delta)
 	if collision != null:
-		if collision.get_collider().get_class() == "StaticBody2D":
-			direction = Vector2(direction.x, -direction.y)
-		if collision.get_collider().get_class() == "CharacterBody2D":
-			direction = Vector2(-direction.x, direction.y)
-			speed *= 1.1
+		velocity = velocity.bounce(collision.get_normal())
