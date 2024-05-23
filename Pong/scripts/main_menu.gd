944e39b629d51254556f25d48d30801134f568a8
Milena Mariz <milenaferreira294@hotmail.com>
2024-05-22 23:40:59 -0300

    feat: open scene by menu button

---
+++ Pong/scripts/main_menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+var game_scene = "res://scenes/game.tscn"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_player_vs_player_button_pressed():
+	get_tree().change_scene_to_file(game_scene)
+	
