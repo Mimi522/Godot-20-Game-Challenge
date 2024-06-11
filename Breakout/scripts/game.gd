d0218b61ad7bff797a03c671b245158a327bd39e
Milena Mariz <milenaferreira294@hotmail.com>
2024-06-10 22:57:59 -0300

    feat: spawn blocks in lines and rows

--- Breakout/scripts/game.gd
+++ Breakout/scripts/game.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
 @export var ball_scene: PackedScene
+@export var block_scene: PackedScene
 @export var score: Label
 
 var menu_scene = "res://scenes/main_menu.tscn"
+
+var start_pos = Vector2(120, 120)
+var delta_x = 152
+var delta_y = 64
 
 const score_txt = "Points: %s"
 
@@ -13,6 +18,7 @@ signal ball_spawned(ball)
 
 func _ready():
 	update_score()
+	spawn_blocks()
 	spawn_ball()
 
 func _process(_delta):
@@ -38,6 +44,14 @@ func despawn_ball(body):
 
 func update_score():
 	score.text = score_txt % [player_score]
+
+func spawn_blocks():
+	for j in range(5):
+		for i in range(7):
+			var block = block_scene.instantiate()
+			block.scale = block.scale * 3
+			block.position = start_pos + Vector2(delta_x * i, delta_y * j)
+			$Blocks.add_child(block)
 
 func _on_back_to_menu_pressed():
 	get_tree().change_scene_to_file(menu_scene)
