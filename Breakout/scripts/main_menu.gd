03ffa60d77cd5117ab5273690535399f8ea5ec77
Milena Mariz <milenaferreira294@hotmail.com>
2024-06-24 20:11:08 -0300

    feat: adjust main menu

--- Breakout/scripts/main_menu.gd
+++ Breakout/scripts/main_menu.gd
@@ -1,9 +1,17 @@
 extends Control
 
-var game_scene = "res://scenes/game.tscn"
+@export var game_menu: Panel
+@export var score_history: Panel
 
-func _on_player_vs_player_button_pressed():
+var game_scene = "res://scenes/game.tscn"	
+
+func _on_play_button_pressed():
 	get_tree().change_scene_to_file(game_scene)
 
-func _on_player_vs_ai_button_pressed():
-	get_tree().change_scene_to_file(game_scene)
+func _on_score_history_button_pressed():
+	game_menu.hide()
+	score_history.show()
+
+func _on_button_pressed():
+	game_menu.show()
+	score_history.hide()
